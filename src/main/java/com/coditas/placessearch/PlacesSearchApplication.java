9d19d8910c82b82fd858042f05c76faced198f6d
pragati-coditas <ajay.jumbad@coditas.com>
2019-05-02 12:19:24 +0530

    Commiting basic skeleton of project

---
+++ src/main/java/com/coditas/placessearch/PlacesSearchApplication.java
@@ -0,0 +1,13 @@
+package com.coditas.placessearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacesSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlacesSearchApplication.class, args);
+	}
+
+}
